71e3bc7db4aa05151ad4b8ca152bf19eb6fabb9f
pengzhendong <275331498@qq.com>
2024-04-25 23:33:30 +0800

    add workflows

--- setup.py
+++ setup.py
@@ -12,22 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from setuptools import setup, find_packages
 
 
-with open("README.md", encoding="utf8") as fin:
-    long_description = fin.read()
-
-with open("requirements.txt", encoding="utf-8") as f:
+with open("requirements.txt", encoding="utf8") as f:
     requirements = f.readlines()
 
 setup(
     name="pyannote-onnx",
-    version=os.getenv("BUILD_VERSION") or "0.0.3",
+    version=open("VERSION", encoding="utf8").read(),
     author="Zhendong Peng",
     author_email="pzd17@tsinghua.org.cn",
-    long_description=long_description,
+    long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
     description="Pyannote ONNX",
     url="https://github.com/pengzhendong/pyannote-onnx",
